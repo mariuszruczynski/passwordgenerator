6dec1e345413dc4075bbbda469d50f48af97fc0b
Mariusz <you@example.com>
2018-06-05 12:10:07 +0200

    add servlet to  PasswordGenerator

--- src/main/java/PasswordGenerator.java
+++ src/main/java/PasswordGenerator.java
@@ -1,4 +1,11 @@
-public class PasswordGenerator {
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(name = "PasswordGenerator", value = "/password")
+public class PasswordGenerator extends HttpServlet {
+
+
+
 
 
 
